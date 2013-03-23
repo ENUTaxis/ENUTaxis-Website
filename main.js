78773d4d307fe7d9c45e176a8bdfa1033acd8673
Guyon David <davidguyon49@gmail.com>
2013-03-23 17:19:05 +0000

    Add: a click on "About" displays the About box
    I used the "alert alert-info" attributes from Bootstrap. I also used the
    click() event from jQuery to show/hide the box.

--- main.js
+++ main.js
@@ -10,3 +10,34 @@ function initialize() {
 	var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 }
 google.maps.event.addDomListener(window, 'load', initialize);
+
+/*
+ * General JS functions
+ */
+$(function() {
+	handleMenuAndBoxes();
+});
+
+function handleMenuAndBoxes() {
+	/*
+	 * Hide all information boxes
+	 */
+	$("#about-box").hide();
+
+	/*
+	 * Close the box when the cross is clicked
+	 */
+	$('.alert .close').click(function(e) {
+		console.log(".alert .close fired");
+    	$(this).parent().hide(400);
+	});
+
+	/*
+	 * About-link displays about-box
+	 */
+	$("#about-link").click(function() {
+		console.log("about-link fired");
+		$("#about-box").show(400);
+		return false;
+	});	
+}

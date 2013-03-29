47dcc2d8bd70c874f03effdc6743c835d3ab8e11
Guyon David <davidguyon49@gmail.com>
2013-03-29 15:34:02 +0000

    Change: forgotten indentation on a PHP file

--- scripts/bookingTableUpdate.php
+++ scripts/bookingTableUpdate.php
@@ -2,5 +2,5 @@
 	mysql_connect("pma.olympe.in","GSn9I4gX","enutaxis","GSn9I4gX");
 	mysqli_query("UPDATE Driver_Table SET Available='N' WHERE (SELECT BookingTime FROM Booking_Table WHERE DATE(SELECT SUBTIME(BookingTime,'0:20:0')) >= NOW()");
 	mysql_query("DELETE FROM Booking_Table WHERE (SELECT DriverID FROM Job_table WHERE finishtime >= NOW())=DriverID");
-mysqli_close();
+	mysqli_close();
 ?>
